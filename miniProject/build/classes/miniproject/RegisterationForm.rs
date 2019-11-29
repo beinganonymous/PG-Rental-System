81bdd5d9c4e2a3a4e0749f7fc5ddbf156e956e87
Pranav Mehta <pranavm182@gmail.com>
2019-11-29 18:13:00 +0530

    Deleted the uploaded files

--- miniProject/build/classes/miniproject/RegisterationForm.rs
+++
@@ -1,6 +0,0 @@
-myloginform.RegisterationForm$2
-myloginform.RegisterationForm$1
-miniProject.RegisterationForm$1
-myloginform.RegisterationForm
-miniProject.RegisterationForm$2
-miniProject.RegisterationForm
